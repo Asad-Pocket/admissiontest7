5a0e0b25a5f6d45979934f6da7be959c599bcaee
hasibul48th <99897658+hasibul48th@users.noreply.github.com>
2022-06-07 23:16:52 +0600

    Update WordCounter.cs

--- Task1/WordCounter.cs
+++ Task1/WordCounter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +8,31 @@ namespace Task1
 {
     public class WordCounter
     {
+        public bool Is_alpha(char ch)
+        {
+            return ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'));
+        }
         public int Count(string text)
         {
-            throw new NotImplementedException();
+
+
+            int Length = text.Length;
+
+            if (Length == 0)
+                return 0;
+
+            int Count = 0;
+            Char[] ch = text.ToCharArray();
+
+            if (Is_alpha(ch[0]))
+                Count++;
+
+            for (int i = 1; i < Length; i++)
+            {
+                if (Is_alpha(ch[i]) && !(Is_alpha(ch[i - 1])))
+                    Count++;
+            }
+            return Count;
         }
     }
 }
