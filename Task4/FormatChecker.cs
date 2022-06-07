f36c7bb1e5d4f0e5d1b57f685e9792af28f1aa72
hasib48thiiuc <59610066+hasib48thiiuc@users.noreply.github.com>
2022-06-07 22:59:05 +0600

    added

---
+++ Task4/FormatChecker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public class FormatChecker
+    {
+        public bool IsValid(string mobileNumber)
+        {
+            int s = 0;
+            int s1 = mobileNumber.Length;
+
+            if(s1!=11)
+                return false;
+            if (mobileNumber[0] == '0' && mobileNumber[1] == '1'  )
+            {
+                for (int i = 2; i <= mobileNumber.Length - 1; i++)
+                {
+                    if (mobileNumber[i] >= 48 && mobileNumber[i] <= 57)
+                        s++;
+
+                }
+
+            }
+            else
+            return false;
+
+            if (s == 9)
+            {
+                return true;
+            }
+            else
+                return false;
+
+            
+
+
+          }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿using Task4;
+
+
+FormatChecker ch1 = new FormatChecker();
+
+string mobileNumber = Console.ReadLine();
+
+if (ch1.IsValid(mobileNumber)) 
+{
+    Console.WriteLine("True");
+
+}
+else
+    Console.WriteLine("False");
